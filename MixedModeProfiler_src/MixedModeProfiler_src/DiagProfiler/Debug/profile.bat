a8daccd265b1a4ee031424813863c09f4d7b3e37
Guangjun Liu <t-gualiu@microsoft.com>
2022-07-29 01:11:51 +0800

    make it more clear

--- MixedModeProfiler_src/MixedModeProfiler_src/DiagProfiler/Debug/profile.bat
+++ MixedModeProfiler_src/MixedModeProfiler_src/DiagProfiler/Debug/profile.bat
@@ -7,10 +7,10 @@ REM CLR Option
 SET COMPLUS_Version=v4.0.30319
 
 REM DiagProfiler
-SET DIAG_PRF_SYMBOLPATH=SRV*c:\symbols*http://msdl.microsoft.com/download/symbols;D:\junFiles\IProfiler\MixedModeProfiler_src\MixedModeProfiler_src\IProfiler\IProfiler\MixedModeProfiler_src\MixedModeProfiler_src\DiagProfiler\Debug;
+SET DIAG_PRF_SYMBOLPATH=SRV*c:\symbols*http://msdl.microsoft.com/download/symbols;D:\junFiles\IProfiler\MixedModeProfiler_src\MixedModeProfiler_src\IProfiler\IProfiler\MixedModeProfiler_src\MixedModeProfiler_src\DiagProfiler\Debug;D:\junFiles\IProfiler\MixedModeProfiler_src\MixedModeProfiler_src\IProfiler\IProfiler\MixedModeProfiler_src\MixedModeProfiler_src\SampleApps\CppCliApp\Debug;
 SET DIAG_PRF_DEBUGTRACE=diag_debugtrace.txt
 SET DIAG_PRF_STACKTRACE=diag_stacktrace.txt
 SET DIAG_PRF_VERBOSE=verbose.txt
 SET DIAG_PRF_ONLY_MANAGED_THREADS=1
-@REM START windbg.exe
-START CppCliApp.exe
+START windbg.exe
+@REM START CppCliApp.exe
